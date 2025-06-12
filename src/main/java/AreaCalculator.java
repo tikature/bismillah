1b031335e8081d90f7a1f121fedbdf6fbe08d194
tikature <detikaa10@gmail.com>
2025-06-12 17:04:03 +0700

    up

---
+++ src/main/java/AreaCalculator.java
@@ -0,0 +1,26 @@
+package src.main.java.ShapeType;
+
+public class AreaCalculator {
+
+	public static double calculateArea(ShapeType shape, double... params) {
+		if (params.length != shape.getNumberOfParams()) {
+			throw new IllegalArgumentException(
+					shape.getName() + " requires " + shape.getNumberOfParams() + " parameter(s).");
+		}
+
+		switch (shape) {
+			case CIRCLE:
+				double radius = params[0];
+				return Math.PI * radius * radius;
+			case SQUARE:
+				double side = params[0];
+				return side * side;
+			case RECTANGLE:
+				double length = params[0];
+				double width = params[1];
+				return length * width;
+			default:
+				throw new IllegalArgumentException("Unsupported shape: " + shape);
+		}
+	}
+}
---
+++ src/main/java/ShapeType.java
@@ -0,0 +1,28 @@
+package src.main.java;
+
+public enum ShapeType {
+	CIRCLE("Circle", 1),
+	SQUARE("Square", 1),
+	RECTANGLE("Rectangle", 2);
+
+	private final String name;
+	private final int numberOfParams;
+
+	ShapeType(String name, int numberOfParams) {
+		this.name = name;
+		this.numberOfParams = numberOfParams;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getNumberOfParams() {
+		return numberOfParams;
+	}
+
+	@Override
+	public String toString() {
+		return name + " (needs " + numberOfParams + " parameter(s))";
+	}
+}
